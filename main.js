11a80e298c1f8578edeff0b86f5474f3524dd14e
Ivan <ivan.dziashchenia@belcommunmash.by>
2023-09-19 16:32:39 +0300

    add gamepad bus control, add signals random view

--- main.js
+++ main.js
@@ -1,29 +1,95 @@
+signalsIds = [
+    'brake_failure', 'park_brake', 'abs', 'stab_ctrl',
+    'engine', 'turn_left', 'stop', 'turn_right',
+    'warning', 'drl', 'beam_low', 'beam_high',
+    'seat_belt', 'asr', 'halt_brake', 'pressure_3',
+    'worn_brake', 'ebs', 'pressure_2', 'ect',
+    'coolant_heat', 'heater_1', 'cab_heater',
+    'mirror_heat', 'window_heat', 'engine_fail',
+    'oil_pressure', 'oil_lvl_max', 'combus_preheat',
+    'side_light', 'fog_front', 'fog_rear', 'bulb_failure',
+    'eng_inlet_air_filter', 'engine_coolant_lvl',
+    'steering_fluid_lvl', 'int_illum_1', 'vent_fan_1',
+    'cab_fan', 'eng_emiss_system_fail', 'adblue',
+    'baby_pram', 'baby_pram1', 'wheelchair',
+    'immobile', 'hill_holding', 'retarder_off',
+    'hydraulic_drive', 'emergency_hammer', 'extinguisher',
+    'tyre_fail', 'return_driver', 'pneumo_1', 'susp_down',
+    'susp_dm', 'transmission_failure', 'transmission_temperature', 'transmission_convert_temperature',
+    'battery', 'pressure_1', 'diesel'
+]
+
 var speed = -30;
 var turns = 30;
 
-var speedContainer = document.getElementById('speed');
-var turnsContainer = document.getElementById('turns');
-var speedDisplay = document.getElementById('speedtext')
-function randspeed() {
-    let newSpeed =  Math.random() * (120 + 30) - 30;
-    speed = newSpeed;
+var bar1 = -30;
+var bar2 = 18;
 
-    speedContainer.style.transform = `rotate(${speed}deg)`
+var oil = 30;
+var temperature = -18;
+
+var speedContainer = document.getElementById('speed');
+
+var turnsContainer = document.getElementById('turns');
+
+var speedDisplay = document.getElementById('speedtext');
+
+var bar1Container = document.getElementById('bar1');
+var bar2Container = document.getElementById('bar2');
+
+var oilContainer = document.getElementById('oil');
+
+var temperatureContainer = document.getElementById('temperature');
+
+for (let i = 0; i < signalsIds.length; i++) {
+    document.getElementById(signalsIds[i]).style.opacity = "0.2";
 }
 
-function randturns() {
-    let newTurns =  Math.random() * (-46 + 30) + 30;
-    turns = newTurns;
+function randSignals() {
+    let randNumber = Math.floor(Math.random() * signalsIds.length);
+    document.getElementById(signalsIds[randNumber]).style.opacity = "1.0";
+    for (let i = 0; i < signalsIds.length; i++) {
+        if (i !== randNumber) {
+            document.getElementById(signalsIds[i]).style.opacity = "0.2";
+        }
+    }
+}
 
-    turnsContainer.style.transform = `rotate(${turns}deg)`
+function randValues() {
+    let newSpeed =  Math.random() * (120 + 30) - 30;
+    let newBar1 = Math.random() * (54 - 18) + 18;
+    let newBar2 = Math.random() * (6 + 30) - 30;
+    let newTurns =  Math.random() * (-46 + 30) + 30;
+    let newOil = Math.random() * (-6 - 30) + 30;
+    let newTemperature = Math.random() * (-54 + 18) - 18;
+
+    speed = newSpeed;
+    bar1 = newBar1;
+    bar2 = newBar2;
+    turns = newTurns;
+    oil = newOil;
+    temperature = newTemperature;
+
+    speedContainer.style.transform = `rotate(${speed}deg)`;
+    bar1Container.style.transform = `rotate(${bar1}deg)`;
+    bar2Container.style.transform = `rotate(${bar2}deg)`;
+    turnsContainer.style.transform = `rotate(${turns}deg)`;
+    oilContainer.style.transform = `rotate(${oil}deg)`;
+    temperatureContainer.style.transform = `rotate(${temperature}deg)`;
 }
 
 function refreshSpeed() {
-    speedDisplay.innerHTML = Math.floor(speed) + 30;
+    speedDisplay.innerHTML = Math.floor((speed + 30) * 0.83);
 }
 
+randSignals()
+
 setInterval(() => {
-    randspeed()
+    randSignals()
+}, 1000)
+
+
+setInterval(() => {
+    randValues()
     refreshSpeed()
-    randturns()
 }, 1000);
---
+++ main2.js
@@ -0,0 +1,126 @@
+
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import Stats from 'three/examples/jsm/libs/stats.module'
+
+const scene = new THREE.Scene()
+const color = 0xFFFFFF;
+const intensity = 5;
+const light = new THREE.DirectionalLight(color, 2);
+const light1 = new THREE.DirectionalLight(color, 2);
+const light2 = new THREE.DirectionalLight(color, 2)
+light.position.set(-1, 2, 4);
+light1.position.set(-1,2,-4);
+light2.position.set(5,12,-4);
+scene.add(light);
+scene.add(light1);
+scene.add(light2)
+
+let rocket;
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    300 / 300,
+    0.1,
+    1000
+)
+camera.position.z = 0
+camera.position.y = 3;
+camera.position.x = -6;
+
+const renderer = new THREE.WebGLRenderer({
+    alpha: true,
+})
+renderer.setSize(300, 300)
+document.querySelector('.speedometer-container').appendChild(renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement)
+
+
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
+
+const objLoader = new GLTFLoader()
+objLoader.load(
+    "./bus_body_red.gltf",
+    // (object) => {
+    //     // (object.children[0] as THREE.Mesh).material = material
+    //     // object.traverse(function (child) {
+    //     //     if ((child as THREE.Mesh).isMesh) {
+    //     //         (child as THREE.Mesh).material = material
+    //     //     }
+    //     // })
+    //     scene.add(object)
+    // },
+    (gltf) => {
+        rocket = gltf.scene;
+        rocket.rotation.y = -300;
+        rocket.position.x = -1;
+        rocket.position.z = 0;
+        scene.add(rocket);
+      }
+)
+
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = 300 / 300
+    camera.updateProjectionMatrix()
+    renderer.setSize(300, 300)
+    render()
+}
+
+function animate() {
+    requestAnimationFrame(animate)
+
+
+
+    controls.update()
+    
+    render()
+}
+
+function render() {
+    renderer.render(scene, camera)
+}
+
+animate()
+
+
+let controllerIndex = null;
+
+window.addEventListener("gamepadconnected", (event) => {
+    const gamepad = event.gamepad;
+    controllerIndex = gamepad.index;
+    console.log("connected");
+});
+
+window.addEventListener("gamepaddisconnected", (event) => {
+    controllerIndex = null;
+    console.log("disconnected");
+});
+
+function handleButtons(buttons) {
+    for (let i = 0; i < buttons.length; i++) {
+        const button = buttons[i];
+
+        if (i == 15 && button.pressed && scene.rotation.y <= 0.2) {
+            scene.rotation.y += 0.01
+            console.log(scene.rotation.y)
+        }
+
+        if (i == 14 && button.pressed && scene.rotation.y >= -0.2) {
+            scene.rotation.y -= 0.01
+            console.log(scene.rotation.y)
+        }
+    }
+}
+
+function gameLoop() {
+    if (controllerIndex !== null) {
+        const gamepad = navigator.getGamepads()[controllerIndex];
+        handleButtons(gamepad.buttons);
+    }
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
